363d9c8f892d69e2dded9b7eb05ec44aec2af26c
Joost Burgers <joostburgers@gmail.com>
2025-01-09 21:40:50 -0500

    first full draft

---
+++ js/faulkner_chart_styles.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+const faulknerChartStyles = {
+    colorway: ["#132C53", "#F27A18", "#ae0700", "#79b473", "#38726c", "#76bed0", "#6b2d5c", "#448b2d", "#e6d812"],
+    colorway_bw: ["#FFFFFF", "#bfbfbf", "#808080", "#404040", "#000000"],
+    paperBackground: "rgba(255,255,255,0)",
+    plotBackground: "rgba(255,255,255,0.4)",
+    margin: { l: 75, r: 50, b: 100, t: 50, pad: 4 },
+    captionStyle: "font-family: 'Playfair Display','Helvetica Neue',Helvetica,Arial,sans-serif; font-weight: normal; font-size:110%;",
+    font: {
+        family: "'Playfair Display','Helvetica Neue',Helvetica,Arial,sans-serif",
+        size: 15,
+        color: '#363636'
+    }
+};
